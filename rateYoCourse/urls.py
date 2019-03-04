611c82f07722f03bf23c63fd9599150cd098406f
kevin0110w <37250646+kevin0110w@users.noreply.github.com>
2019-03-04 11:41:44 +0000

    spelling to urlpatterns

--- rateYoCourse/urls.py
+++ rateYoCourse/urls.py
@@ -2,7 +2,7 @@
 
 
 
-urlpatters = [
+urlpatterns = [
 	url(r'^register/$', views.register, name='register'),
 	url(r'^login/$', views.user_login, name='login'),
 	url(r'^logout/$', views.user_logout, name='logout'),
