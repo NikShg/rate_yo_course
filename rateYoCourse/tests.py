e6020eada98abe1bc700fb1e95d081df89ad67cf
NikShg <nikidde@outlook.com>
2019-03-20 20:19:56 +0000

    some tests

--- rateYoCourse/tests.py
+++ rateYoCourse/tests.py
@@ -9,6 +9,7 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.test import LiveServerTestCase
 import populate_rateYoCourse
 from datetime import datetime, timedelta
+import socket
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -20,159 +21,57 @@ from selenium.webdriver.common.keys import Keys
 from django.contrib.auth.models import User
 from rateYoCourse.models import UserProfile
 from django.template import loader
-from django.conf import seetings
+from django.conf import settings
 
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.core.files.storage import default_storage
 
+from django.contrib.staticfiles import finders
+
 # Model tests
 
-class UniversityCourseMethodTests(TestCase):
-	def test_slug_line_creation_university(self):
-		university = University('Glasgow University')
-		university.save()
-		self.assertEqual(university.slug,'glasgow-university')
+class testView(TestCase):
+	def test_index(self):
+		response = self.client.get(reverse('index'))
+		self.assertIn('Welcome to Rate Yo Course'.lower(), response.content.decode('ascii').lower())
 	
-	def test_slug_line_creation_course(self):
-		course = Course('Internet Technology')
-		course.save()
-		self.assertEqual(course.slug,'glasgow-university')
+	def test_about_contains_create_message(self):
+		self.client.get(reverse('index'))
+		response = self.client.get(reverse('about'))
+		self.assertIn('About our team'.lower(),response.content.decode('ascii').lower())
 
-class IndexViewTests(TestCase):
+class testView2(TestCase):
+	def test_about_using_template(self):
+		self.client.get(reverse('index'))
+		response = self.client.get(reverse('about'))
 
-	def test_university_with_no_courses(self):
-		response= self.client.get(reverse('index'))
-		self.assertEqual(response.status_code,200)
-		self.assertContains(response,"There are no universities present.")
-		self.assertQuerysetEqual(response.context['universities'],[])
+		self.assertTemplateUsed(response,'rateyocourse/about.html')
 	
-	def add_course(uni, name, url, rating): #, rating=0
-		c = Course.objects.get_or_create(university=uni, name=name)[0]
-		c.url=url
-		c.rating = rating
-		c.save()
-		return c
+	def test_static_files(self):
+		result = finders.find('images/team.jpg')
+		self.assertIsNotNone(result)
 
-	def add_uni(name, city, url): #city, url
-		u = University.objects.get_or_create(name=name)[0]
-		u.city=city
-		u.url=url
-		u.save()
-		return u
-	
-	def test_university_with_universities(self):
-		add_uni('Glasgow','Glasgow','https://www.gla.ac.uk/')
-		add_uni('Edinburgh', 'Edinburgh', 'https://www.ed.ac.uk/')
+class test_models(TestCase):
+	def test_create_a_new_uni(self):
+		uni = University(name="Glasgow")
+		uni.save()
 
-		response = self.client.get(reverse('universities'))
-		self.assertEqual(response.status_code, 200)
-		self.assertContains(response, 'Edinburgh')
+		uni_in_data = University.objects.all()
+		self.assertEquals(len(uni_in_data),1)
+		only = uni_in_data[0]
+		self.assertEquals(only, uni)
 
-		num_uni = len(response.context['universities'])
-		self.assertEqual(num_uni,2)
+	def test_create_a_course_for_unis(self):
+		uni = University(name="Glasgow")
+		uni.save()
 
-	def test_uni_with_courses(self):
-		add_course('Glasgow', 'ITECH','https://www.gla.ac.uk/coursecatalogue/course/?code=COMPSCI5012')
-		add_course('Glasgowm', 'Team Project', 'https://www.gla.ac.uk/coursecatalogue/course/?code=COMPSCI5074')
-	
-		response = self.client.get(reverse('university'))
-		self.assertEqual(response.status_code, 200)
-		self.assertContains(response, 'ITECH')
+		course_data = Course()
+		course_data.uni= uni
+		course_data.uni.name="ITECH"
+		course_data.uni.url="https://www.gla.ac.uk/postgraduate/taught/informationtechnology/?card=course&code=COMPSCI5012"
+		course_data.save()
 
-		num_c = len(response.context['courses'])
-		self.assertEqual(num_c,2)
+		course_data = uni.course.set_all()
+		first_page = course_data[0]
 
-
-# sil test  pip install selenium==2.33.0
-class TestS(TestCase):
-	def setUp(self):
-		from django.contrib.auth.models import User
-		User.objects.create_superuser(username='admin',password='admin!',email='admin@mail.ru')
-		chrome_options = webdriver.ChromeOption()
-		chrome_options.add_argument('headless')
-		self.browser = webdriver.Chrome(chrome_options = chrome_options)
-		self.browser.implicitly_wait(3)
-	def td(self):
-		self.browser.refresh()
-		self.browser.quit()
-	
-	def test_register_user(self):
-		url = self.live_server_url
-		url = url.replace('localhost','http://127.0.0.1:8000')
-
-		try:
-			self.browser.get(url + reverse('index'))
-		except:
-			try:
-				self.browser.get(url + reverse ('rateyocourse:index'))
-			except:
-				return False
-		self.browser.find_elements_by_link_text('Login')[0].click()
-
-		username_field = self.browser.find_element_by_name('username')
-		username_field.send_keys('testuser')
-
-		email
-	#def setUp(self):
-	#	self.driver = webdriver.Firefox()
-	
-	#def test_login(self):
-	#	self.driver.get('http://127.0.0.1:8000/accounts/login/')
-
-	#	expectedMessage = "You are not signed in, this means you will not be able to rate courses and universities"
-	#	message = driver.findElement(By.xpath("//div[contains(@class,'callout callout-failure')]")).getText();
-	#	self.assertTrue("Your error message", message.contains(expectedMessage));
-		#self.driver.select_from(nr=0)
-		#self.driver.form['username']='Zxc'
-		#self.driver.form['password']='Zxc123'
-		#self.driver.submit()
-
-	#def test_index_search(self):
-		#self.driver.get("http://127.0.0.1:8000")
-		#assert 'rateyocourse' in browser.title
-
-
-	
-		
-		#search = browser.find_element_by_name('q')
-		# search.send_keys("Internet Technology")
-		#search.send_keys(Keys.RETURN)
-		#time.sleep(5)
-		#browser.quit()
-
-class Chapter10SessionTests(TestCase):
-	def test_about_page_shows_number_of_visits(self):
-		try:
-			response=self.client.get(reverse('index'))
-		except:
-			try:
-				response=self.client.get(reverse('rateyocourse:index'))
-			except:
-				return False
-		try:
-			response=self.client.get(reverse('about'))
-		except:
-			try:
-				response=self.client.get(reverse('rateyocourse:about'))
-			except:
-				return False
-
-	def test_user_access(self):
-		for i in range(0,100):
-			try:
-				response = self.client.get(reverse('index'))
-			except:
-				try:
-					response=self.client.get(reverse('rateyocourse:index'))
-				except:
-					return False
-			session = self.client.session
-			self.assertIsNotNone(self.client.session['visits'])
-			self.assertIsNotNone(self.client.session['last_visit'])
-
-			last_visit = datetime.noew() - timedelta(days=1)
-
-			session['last_visit']= str(last_visit)
-			session.save()
-
-			self.assertEquals(session['visits'],session['visits'])
+		self.assertEquals(first_page)
