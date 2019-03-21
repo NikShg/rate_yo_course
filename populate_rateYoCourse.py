878823d4516a8c7167de907c065f05dba4c99617
Kevin Wu <0808148w@student.gla.ac.uk>
2019-03-21 11:48:06 +0000

    editing the step 2 registration page

--- populate_rateYoCourse.py
+++ populate_rateYoCourse.py
@@ -3,30 +3,35 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rate_yo_course.settings')
 
 import django
 django.setup()
-from rateYoCourse.models import University, Course
+from rateYoCourse.models import University, Course, UserProfile, Comment
 
 
 def populate():
-    University_of_Glasgow_courses = [
+	University_of_Glasgow_courses = [
         {"name": "Internet Technology", "url": "https://www.gla.ac.uk/postgraduate/taught/informationtechnology/?card=course&code=COMPSCI5012", "rating": 5},
         {"name": "Advanced Programming", "url": "https://www.gla.ac.uk/postgraduate/taught/softwaredevelopment/?card=course&code=COMPSCI5002", "rating": 3}
     ]
 
-    University_of_Edinburgh_courses = [
+	University_of_Edinburgh_courses = [
+        {"name":"Computer Science BSc", "url": "https://www.ed.ac.uk/studying/undergraduate/degrees/index.php?action=programme&code=G400", "rating": 0},
+        {"name":"Artificial Intelligence BSc", "url": "https://www.ed.ac.uk/studying/undergraduate/degrees/index.php?action=programme&code=G700", "rating": 1},
+    ]
+	
+	University_of_Edinburgh_courses = [
         {"name":"Computer Science BSc", "url": "https://www.ed.ac.uk/studying/undergraduate/degrees/index.php?action=programme&code=G400", "rating": 0},
         {"name":"Artificial Intelligence BSc", "url": "https://www.ed.ac.uk/studying/undergraduate/degrees/index.php?action=programme&code=G700", "rating": 1},
     ]
 
-    unis = {"University of Glasgow": {"courses": University_of_Glasgow_courses, "city": "Glasgow", "url": "https://www.gla.ac.uk"}, "University of Edinburgh": {"courses": University_of_Edinburgh_courses, "city": "Edinburgh", "url": "https://www.ed.ac.uk"}}
-
-    for uni, uni_data in unis.items():
-        u = add_uni(uni, uni_data["city"], uni_data["url"]) #city, url
-        for c in uni_data["courses"]:
-            add_course(u, c["name"], c["url"], c["rating"])
-
-    for u in University.objects.all():
-        for c in Course.objects.filter(university=u):
-            print("- {0} - {1}".format(str(u), str(c)))
+	unis = {"University of Glasgow": {"courses": University_of_Glasgow_courses, "city": "Glasgow", "url": "https://www.gla.ac.uk"}, "University of Edinburgh": {"courses": University_of_Edinburgh_courses, "city": "Edinburgh", "url": "https://www.ed.ac.uk"}}
+	
+	for uni, uni_data in unis.items():
+		u = add_uni(uni, uni_data["city"], uni_data["url"]) #city, url
+		for c in uni_data["courses"]:
+			add_course(u, c["name"], c["url"], c["rating"])
+					
+	for u in University.objects.all():
+		for c in Course.objects.filter(university=u):
+			print("- {0} - {1}".format(str(u), str(c)))
 
 def add_course(uni, name, url, rating): #, rating=0
 	c = Course.objects.get_or_create(university=uni, name=name)[0]
@@ -41,6 +46,18 @@ def add_uni(name, city, url): #city, url
 	u.url=url
 	u.save()
 	return u
+	
+def add_user(user, about, status, picture):
+	user = UserProfile.objects.get_or_create(user=user)[0]
+	user.about = about
+	user.status = status
+	user.picture = picture
+	return user
+
+def add_comment(university, course, user, body):
+	comment = Comment.objects.get_or_create(university=university, course=course, user=user)[0]
+	comment.body = body
+	return comment
 
 if __name__ == '__main__' :
 	print("Starting rateYoCourse population script...")
